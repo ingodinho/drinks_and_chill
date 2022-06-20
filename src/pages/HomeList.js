8f0b1677eb902d5897a298b8703203ccb84f8371
CipiVlad <supercoder.cipi@gmail.com>
2022-06-20 12:57:28 +0200

    xx

--- src/pages/HomeList.js
+++ src/pages/HomeList.js
@@ -4,24 +4,22 @@ const HomeList = () => {
 
     // const link = 'www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka';
     // const link = 'www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
-    const link = 'www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka';
-    const [cocktails, setCocktails] = useState([]);
+    // const link = 'www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka';
+    // const [cocktails, setCocktails] = useState([]);
 
-    fetch(link)
-        .then(response => {
-            console.log(response);
-            return response.json()
-        })
-        .then(json => console.log(json))
+    // fetch("www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    //     .then(response => response.json())
+    //     .then(json => console.log(json.drinks))
 
-    // useEffect( () => {
-    //     fetch(link)
-    //     .then(response => {
-    //         console.log(response);   
-    //         return response.json()
-    //     })
-    //     .then(json => console.log(json))
-    // }, [])
+    const [drink, setDrink] = useState([]);
+
+    useEffect(() => {
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+            .then((res) => res.json())
+            .then((json) => setDrink(json.drinks));
+    }, []);
+    console.log(drink);
+
 
     return (
         <div>TEST</div>
