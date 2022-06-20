914113b3cf43b52bd00d87326ec36f523b448b2b
ingodinho <siemens.ingo@googlemail.com>
2022-06-20 11:14:29 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.scss';
+import {Routes, Route} from 'react-router-dom';
+import HomeList from './pages/HomeList';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<HomeList />}/> 
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/HomeList.js
@@ -0,0 +1,31 @@
+import {useState, useEffect} from 'react';
+
+const HomeList = () => {
+
+    // const link = 'www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka';
+    // const link = 'www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+    const link = 'www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka';
+    const [cocktails, setCocktails] = useState([]);
+
+    fetch(link)
+        .then(response => {
+            console.log(response);   
+            return response.json()
+        })
+        .then(json => console.log(json))
+
+    // useEffect( () => {
+    //     fetch(link)
+    //     .then(response => {
+    //         console.log(response);   
+    //         return response.json()
+    //     })
+    //     .then(json => console.log(json))
+    // }, [])
+
+    return (
+        <div>TEST</div>
+    )
+}
+
+export default HomeList;
