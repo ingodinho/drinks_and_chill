caa745e854b584c46f39384a6cb5dbefebf56bdd
Kadi <gueneykadircan@gmail.com>
2022-06-21 16:46:08 +0200

    pushy

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import { Routes, Route, useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import HomeList from './pages/HomeList';
 import CocktailList from './pages/CocktailList';
-<<<<<<< HEAD
 import Header from './components/Header';
 import Footer from './components/Footer';
-=======
 import { AnimatePresence } from 'framer-motion';
 import AddCocktail from './pages/AddCocktail';
 import Search from './components/Search';
 import Navbar from './components/Navbar';
->>>>>>> 60c7d10c8609bc3575943901685ca4fae1502cd1
 
 function App() {
 	const [search, setSearch] = useState('');
@@ -25,19 +22,11 @@ function App() {
 		setValid(isValid);
 	};
 
-<<<<<<< HEAD
-	return (
-		<div className='App'>
-			<Routes>
-				<Route path='/' element={<HomeList />} />
-				<Route path='/cocktails' element={<CocktailList />} />
-			</Routes>
-=======
 	const location = useLocation();
 
 	return (
 		<div className='App'>
-
+			<Header />
 			{location.pathname !== '/addCocktail' && (
 				<Search searchHandler={searchHandler} valid={valid} />
 			)}
@@ -56,7 +45,7 @@ function App() {
 					<Route path='/addCocktail' element={<AddCocktail />} />
 				</Routes>
 			</AnimatePresence>
->>>>>>> 60c7d10c8609bc3575943901685ca4fae1502cd1
+			<Footer />
 		</div>
 	);
 }
--- src/pages/HomeList.js
+++ src/pages/HomeList.js
@@ -1,35 +1,24 @@
 import categories from '../components/categories';
 import HomeListItems from '../components/HomeListItems';
-<<<<<<< HEAD
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-=======
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
->>>>>>> 60c7d10c8609bc3575943901685ca4fae1502cd1
 
 const HomeList = () => {
 
-<<<<<<< HEAD
 	return (
-		<main>
-			<Header />
-			<div className='home_list list_grid'>
-				<input
-					type='text'
-					placeholder='type something'
-					onChange={(event) => {
-						setFilter(event.target.value);
-					}}
-				/>
-				<Link
-					to='/cocktails'
-					state={`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${filter}`}
-				>
-					<button>search</button>
-				</Link>
 
+		<motion.div
+			className='home_list'
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			key={1}
+		>
+			<Navbar />
+			<div className='list_grid'>
 				{categories.map((el, i) => (
 					<HomeListItems
 						className={`style${Math.floor((i % 6) + 1)} ${i % 2 === 0 ? 'left' : 'right'
@@ -38,40 +27,12 @@ const HomeList = () => {
 						name={el.name}
 						desc={el.description}
 						url={el.url}
-						filter={filter}
 					/>
 				))}
 			</div>
-			<Footer />
-		</main>
-	);
-=======
-    return (
-
-        <motion.div
-            className='home_list'
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            key={1}
-        >
-            <Navbar />
-            <div className='list_grid'>
-                {categories.map((el, i) => (
-                    <HomeListItems
-                        className={`style${Math.floor((i % 6) + 1)} ${i % 2 === 0 ? 'left' : 'right'
-                            }`}
-                        key={el.id}
-                        name={el.name}
-                        desc={el.description}
-                        url={el.url}
-                    />
-                ))}
-            </div>
-            <Link to="/addCocktail" className='addcoke'>Add Cocktail</Link>
-        </motion.div>
-    )
->>>>>>> 60c7d10c8609bc3575943901685ca4fae1502cd1
+			<Link to="/addCocktail" className='addcoke'>Add Cocktail</Link>
+		</motion.div>
+	)
 };
 
 
