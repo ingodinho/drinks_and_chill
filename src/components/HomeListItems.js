069ff9d8505ef181b3c0a95f7eb079f0c6e9cb25
ingodinho <siemens.ingo@googlemail.com>
2022-06-22 16:36:50 +0200

    searchbar with suggestions

--- src/components/HomeListItems.js
+++ src/components/HomeListItems.js
@@ -1,8 +1,13 @@
 import { Link } from 'react-router-dom';
 
 const HomeListItems = (props) => {
+
+	const onClickHandler = (e) => {
+		props.searchHandler('')
+	}
+
 	return (
-		<Link to={'/cocktails'} state={props.url}>
+		<Link to={'/cocktails'} state={props.url} onClick={onClickHandler}>
 			<div className={`home_list_item ${props.className}`} >
 				<h2>{props.name}</h2>
 				<p>{props.desc}</p>
