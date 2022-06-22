fd15ad103a5ddf3a23e9cb83fde72544495737fb
ingodinho <siemens.ingo@googlemail.com>
2022-06-22 09:10:54 +0200

    diesdas

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
-import { useState} from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useLocation, useNavigate } from 'react-router-dom';
-import {CaretDoubleDown} from 'phosphor-react';
+import { CaretDoubleDown } from 'phosphor-react';
 import './Search.scss';
 
 const Search = (props) => {
@@ -10,11 +10,11 @@ const Search = (props) => {
 	const location = useLocation();
 	const navigate = useNavigate();
 	const onchangeHandler = (e) => {
-		if(e.target.value === '') {
+		if (e.target.value === '') {
 			navigate('/');
 			return;
 		}
-		if(location.pathname === '/cocktails') {
+		if (location.pathname === '/cocktails') {
 			props.searchHandler(e.target.value);
 		} else if (location.pathname === '/') {
 			navigate('/cocktails');
@@ -22,29 +22,34 @@ const Search = (props) => {
 		}
 	};
 	return (
-		<>
+		<section className='search'>
 			<input
 				type='text'
 				placeholder='type something'
 				onChange={onchangeHandler}
 				style={{ color: !props.valid ? 'red' : 'black' }}
 				value={props.value}
+				className='input_feld'
 			/>
 			<Link
 				to='/cocktails'
 				state={`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${filter}`}
 			>
-				<button>search</button>
+				<button className='search_button'>search</button>
 			</Link>
 			{!props.valid && (
 				<p style={{ color: 'black' }}>
 					Eingabe hat keine neuen Treffer ergeben
 				</p>
 			)}
-			<div className="caret_double">
-				<CaretDoubleDown size={32} color="#fff" className='caret_double_icon'/>
+			<div className='caret_double'>
+				<CaretDoubleDown
+					size={32}
+					color='#fff'
+					className='caret_double_icon'
+				/>
 			</div>
-		</>
+		</section>
 	);
 };
 
